648cace093dd09625b3a6185be9e6079bd97a9b5
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-13 16:10:36 +0000

    added nav bar to header with css styling

--- src/components/Header/Nav/Header.js
+++ src/components/Header/Nav/Header.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
     return (
         <header>
             <section className="navigation">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <a class="navbar-brand spacer" href="#">Sarah Ellis</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                    <a class="nav-link spacer" href="#anchor-projects">My Projects</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="">Projects</a>
+                    <a class="nav-link spacer" href="#anchor-skills">Skills</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="">Contact</a>
+                        <a class="nav-link spacer" href="#anchor-contact">Contact</a>
                     </li>
-                    </ul>
+                </ul>
                 </div>
-                </nav>
+            </nav>
             </section>
         </header>
     )
