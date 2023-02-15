f1b757c4e4746df0c1bb4f56d9e0373c52a7d0e0
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-15 19:40:14 +0000

    updated styling to jumbotron css styling

--- src/components/Header/Nav/Header.js
+++ src/components/Header/Nav/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header>
             <section className="navigation">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand spacer" href="#">Sarah Ellis || Web Developer</a>
+                <a className="navbar-brand spacer" href="#">Sarah Ellis</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
