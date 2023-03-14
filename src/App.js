1175b518b27f53cc2cfa0a069f7b7bc6148f658f
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-03-14 19:14:22 +0000

    added grid section for portfolio cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header/Nav/Header';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
+import Projects from './components/Projects-Section/Projects-Section'
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Projects />
       <Footer />
     </div>
   );
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,9 @@ import profileImage from '../Assets/Sarah.jpeg'
 
 function Hero() {
     return (
-        <section class="jumbotron">
-            <h1 class="display-4 spacer">Sarah Ellis Web Developer</h1>
-            <img src={profileImage} class="profile-image" alt="Sarah Ellis"></img>
+        <section className="jumbotron">
+            <h1 className="display-4 spacer">Sarah Ellis Web Developer</h1>
+            <img src={profileImage} className="profile-image" alt="Sarah Ellis"></img>
         </section>
     )
 }
